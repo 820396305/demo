023ad8e12e58ba3abdfdab57c6c642b52b6b0050
max <820396305@qq.com>
2016-01-23 15:42:03 +0800

    delete some

--- fileupload/fileupload.js
+++ fileupload/fileupload.js
@@ -1,6 +1,6 @@
  /* =========== window.IO 文件上传 ===========*/    
 (function(){
-	function fileUpload(){
+	function fileUpload(single){
 		this.dom = document.createElement('input');
 		this.dom.type='file';
 		var _this = this,_files = [],index = 0;
@@ -31,12 +31,14 @@
 			}
 			return null;
 		}
+		//根据file.index得到在数组中的索引
 		this.getFileIndex = function(ix){
 			for(var i=0;i<_files.length;i++){
 				if(_files[i].index==ix) return i;
 			}
 			return -1;
 		}
+		//获取待上传文件数组 filter?所有正确文件:所有文件(包括过滤掉的)
 		this.getFiles = function(filter){
             if(filter){     //过滤错误文件
                 var _filesArr = [];
@@ -47,6 +49,7 @@
             }
             return _files;
         };
+        //获取所有文件总大小 filter?所有正确文件:所有文件(包括过滤掉的)
         this.getSize = function(filter){
             var sum = 0;
             if(filter){     //过滤错误文件
@@ -63,6 +66,7 @@
 		
 		this.upload = function(url,name,args,callback,failcallback){
 			name = name || "upload[]";
+			args = args || {};
 			if(_files.length<=0){ failcallback && failcallback({status:-1,statusText:"未选择文件"});return; }
 			var formData = new FormData();
 			for(var i= 0;i<_files.length;i++){
@@ -96,29 +100,29 @@
 			_files = [];
 			callback && callback();
 		};
-		this.change = function(callback,filter,fileEle){
+		this.change = function(callback,filter,element){
 			this.dom.onchange = function(){
 				var files = _this.dom.files;
 				_change(files);
 			};
-			if(fileEle){
+			if(element){
 				//拖动上传
-				fileEle.ondragenter=function(e) {
+				element.ondragenter=function(e) {
 					e.preventDefault();
 				}
-				fileEle.ondragover=function(e) {
+				element.ondragover=function(e) {
 					e.preventDefault();
 				}
-				fileEle.ondragleave=function(e) {
+				element.ondragleave=function(e) {
 					e.preventDefault();
 				}
-				fileEle.ondrop=function(e) {
+				element.ondrop=function(e) {
 					var files = e.dataTransfer.files;
 					_change(files);
 					e.preventDefault();
 				}
-				//粘贴上传
-				fileEle.onpaste = function(e){
+				//粘贴上传（如聊天框发送图片）
+				element.onpaste = function(e){
 					var clipboard = e.clipboardData;
 				    // 有无内容
 				    if(!clipboard.items || !clipboard.items.length){
@@ -132,7 +136,6 @@
 				    }
 				}
 			}
-
 			function _change(files){
 				if(files.length){
 					for(var ig=0;ig<files.length;ig++){
@@ -159,9 +162,19 @@
 								}
 							}
 						}
-						files[ig].index = ++index;
-						_files.push(files[ig]);
-						callback(files[ig],errormsg.toString());
+						if(single){
+							if(!files[ig].error){
+								files[ig].index = ++index;	
+								 _files = [files[ig]];
+								callback(files[ig]);	
+								 _this.dom.value = '';
+								 return;
+							 }
+						}else{
+							files[ig].index = ++index;
+							_files.push(files[ig]);
+							callback(files[ig],errormsg.toString());	
+						}
 					}
 					_this.dom.value = '';
 				}
@@ -176,6 +189,7 @@
 		this.chooseCammar = function(){
 			this.chooseFiles("video/*;capture=camcorder");
 		}
+		//直接获取 图片<img> Dom
 		this.getImage = function(file,classname){
 			file = file.index ? file : this.getFile(file);
 			if(!file || file.type.indexOf('image')!=0){
@@ -206,6 +220,7 @@
 			}
 			return img;
 		}
+		//获取文件文本信息
 		this.getText = function(file,callback){
 			file = file.index ? file : this.getFile(file);
 			if(!file || !window.FileReader){
@@ -217,6 +232,7 @@
             };
            	reader.readAsText(file);
 		}
+		//获取文临时路径
 		this.getImageSrc = function(file,callback){
 			file = file.index ? file : this.getFile(file);
 			if(!file || file.type.indexOf('image')!=0){
@@ -248,6 +264,41 @@
 			}else{
 				return null;
 			}
+		}
+		//需要导入exif.min.js才能使用该方法
+		//返回内容 {name:'图片名称',photo_at:'拍照时间',latitude:'拍摄地经度',latitude:'拍摄地纬度'}
+		this.getImageBaseExif = function(file,callback){
+			if(!file || !EXIF){
+				callback && callback(null);
+				return;
+			}
+			EXIF.getData(file, function() {
+				var obj ={name:file.name || '',photo_at:'',latitude:'',longitude:''};
+				var lngExif = this.exifdata.GPSLongitude;
+				var latExif = this.exifdata.GPSLatitude;
+				obj.photo_at = this.exifdata.DateTimeOriginal;
+				if(!obj.photo_at){
+					callback && callback(null);
+					return;
+				}
+				if(!lngExif || !latExif){
+					callback && callback(null);
+					return;
+				}
+		        obj.longitude = lngExif[0].numerator/lngExif[0].denominator+(lngExif[1].numerator/lngExif[1].denominator)/60+(lngExif[2].numerator/lngExif[2].denominator)/3600;
+		        obj.latitude = latExif[0].numerator/latExif[0].denominator+(latExif[1].numerator/latExif[1].denominator)/60+(latExif[2].numerator/lngExif[2].denominator)/3600;
+		        callback &&　callback(obj);
+		    });
+		}
+		//需要导入exif.min.js才能使用该方法
+		this.getImageExif = function(file,callback){
+			if(!file || !EXIF){
+				callback && callback(null);
+				return;
+			}
+			EXIF.getData(file, function() {
+				callback && callback(this.exifdata);
+		    });
 		}
 	}
 	window.IO = $.extend(window.IO,{fileUpload:fileUpload});
